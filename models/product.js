386f990d756a0af3c5e5d48a68ac0f0772bc137e
oacostam <oscar.acostamontesde@gmail.com>
2016-06-05 22:26:36 +0200

    CORS added

--- models/product.js
+++ models/product.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 
 exports.productSchema = new mongoose.Schema({
-    name: {String, required: true},
+    name: {type: String, required: true},
     createdOn: { type: Date, 'default': Date.now, required: true },
     modifiedOn: { type: Date, default: Date.now, required: true },
     createdBy: String,
-    barcode: {String, required: true},
+    barcode: {type: String, required: true},
     categories: [String],
     picture : Buffer,
-    isActive: {Boolean, 'default': true, required: true},
-    currentAmount : {Number, 'default' : 0}
+    isActive: {type: Boolean, 'default': true, required: true},
+    currentAmount : {type: Number, 'default' : 0}
 });
